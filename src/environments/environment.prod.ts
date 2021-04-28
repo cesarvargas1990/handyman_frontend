faacc09a10f2bbe99b28fc4dc84399408d2f2ffc
cesar augusto <djkav2009@gmail.com>
2021-04-27 21:44:17 -0500

    ajustes environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl : 'http://ec2-18-188-180-70.us-east-2.compute.amazonaws.com:8080/api/report',
+  serverUrl: 'http://ec2-18-188-180-70.us-east-2.compute.amazonaws.com:8080'
 };
